cf94caca34c8be09526711ff6855ad84adc03465
Joshua Mclean <joshmcleana@gmail.com>
2020-03-03 03:47:26 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,9 +1,13 @@
 require 'yaml'
-require 'pry'
+require 'pry' #add require pry
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+# 1st time I added binding.pry
+#then i wrote the below loop code to (traps pry in loop'?)
+
 
 # spielberg_movies = directors_database[0][:movies]
 # row_index = 0
